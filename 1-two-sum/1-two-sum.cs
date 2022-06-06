450d22bf48b16a4a87a594c516b966cca8b53e07
Shubham Roy <36968449+ShubhamRoy96@users.noreply.github.com>
2022-06-06 11:11:25 +0530

    Time: 189 ms (66.94%), Space: 43.8 MB (34.88%) - LeetHub

--- 1-two-sum/1-two-sum.cs
+++ 1-two-sum/1-two-sum.cs
@@ -2,15 +2,20 @@ public class Solution {
     public int[] TwoSum(int[] nums, int target) {
         var i = 0;
         var j = 0;
+        var result = new int[2];
         for(i = 0; i < nums.Length; i++)
         {
             for(j = i + 1; j < nums.Length; j++)
             {
                 if(nums[i] + nums[j] == target)
-                    return new int[]{i,j};
+                {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
             }
         }
         
-        return null;
+        return result;
     }
 }
