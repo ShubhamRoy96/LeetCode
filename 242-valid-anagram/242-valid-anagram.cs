cc4107cb9ff88a39a1b9230003bb30febf90f8c9
Shubham Roy <36968449+ShubhamRoy96@users.noreply.github.com>
2022-06-06 11:53:48 +0530

    Time: 130 ms (25.60%), Space: 39.5 MB (40.80%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cs
+++ 242-valid-anagram/242-valid-anagram.cs
@@ -1,6 +1,11 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        return string.Concat(s.OrderBy(c=> c)) == string.Concat(t.OrderBy(c => c));
-        
+        var firstStrArr = s.ToArray();
+        Array.Sort(firstStrArr);        
+        var firstStrSorted = new string(firstStrArr);
+        var secondStrArr = t.ToArray();
+        Array.Sort(secondStrArr);        
+        var secondStrSorted = new string(secondStrArr);
+        return firstStrSorted == secondStrSorted;
     }
 }
